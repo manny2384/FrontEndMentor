83e36c4ad9e57e16e50dd3a5a0a4e0c87adcdacc
manny2384 <gonzalezeh2015@gmail.com>
2021-12-20 10:08:51 -0800

    mobile init

--- nft/src/App.js
+++ nft/src/App.js
@@ -15,10 +15,10 @@ function App() {
             
             <div className="nftImage"> 
               <img id="nft" alt="nftImage" src={equilibrium} /> 
-              <a href=""><img id="eye" src={view} alt="eye" /></a>
+              <a href="#"><img id="eye" src={view} alt="eye" /></a>
             </div>
 
-            <div className="tag"> <a> Equilibrium #3429 </a> </div>
+            <div className="tag"> <a href="#"> Equilibrium #3429 </a> </div>
 
             <div className="desc"> Our Equilibrium collection promotes balance and calm. </div>
 
@@ -28,7 +28,7 @@ function App() {
             </div>
 
 
-            <div className="author"> <img src={avatar} alt="avatar" /> <p> Creation of <a href="">Jules Wyvern</a> </p> </div>
+            <div className="author"> <img src={avatar} alt="avatar" /> <p> Creation of <a href="#">Jules Wyvern</a> </p> </div>
           </div>
 
         </div>
